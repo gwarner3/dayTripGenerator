160c955f5fbb1c781b9c06c6373011d0d87bc9b2
gwarner3 <george@georgedevelops.com>
2023-03-19 13:17:22 -0500

    Added main.py and lists

---
+++ main.py
@@ -0,0 +1,4 @@
+restaurante = ["just natural", "mcdoanlds", "wendys", "sweet spot"]
+destinations = ["Sao Paulo, Brazil", "Accra, Ghana", "Negril, Jamaica", "San Juan, Costa Rica"]
+transportation = ["Lyft", "Uber", "walking", "biking"]
+entertainment = ["sightseeing", "kayaking", "sunbathing", "hiking"]
